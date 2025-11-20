138270637773f4e1aa7b29c64863b86dc7273545
szuryuu <ilhamdzaky2007@gmail.com>
2025-11-20 13:30:57 +0700

    fix: member view

---
+++ fe/src/components/ui/input-group/InputGroupButton.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { inputGroupButtonVariants } from ".";
+const props = defineProps({
+  variant: { type: null, required: false, default: "ghost" },
+  size: { type: null, required: false, default: "xs" },
+  class: { type: null, required: false }
+});
+</script>
+
+<template>
+  <Button
+    :data-size="props.size"
+    :variant="props.variant"
+    :class="cn(inputGroupButtonVariants({ size: props.size }), props.class)"
+  >
+    <slot />
+  </Button>
+</template>
--- fe/src/views/MemberView.vue
+++ fe/src/views/MemberView.vue
@@ -1,5 +1,70 @@
-<script setup></script>
+<script setup>
+import { SearchIcon, TextAlignCenter } from 'lucide-vue-next'
+
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupInput,
+} from '@/components/ui/input-group'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+const members = [
+  {
+    id: 'INV001',
+    name: 'Paid',
+    position: '$250.00',
+    departement: 'Credit Card',
+  },
+]
+</script>
 
 <template>
-  <div>coba</div>
+  <header class="px-1.5 flex justify-between">
+    <div>Management Anggota</div>
+    <div>
+      <InputGroup>
+        <InputGroupInput placeholder="Search Anggota" />
+        <InputGroupAddon>
+          <SearchIcon />
+        </InputGroupAddon>
+        <InputGroupAddon align="inline-end">
+          <InputGroupButton><TextAlignCenter /></InputGroupButton>
+        </InputGroupAddon>
+      </InputGroup>
+    </div>
+  </header>
+  <main>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead class="w-[100px]"> No </TableHead>
+          <TableHead>Nama</TableHead>
+          <TableHead>Jabatan</TableHead>
+          <TableHead>Departement</TableHead>
+          <TableHead class="text-right capitalize"> action </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow v-for="member in members" :key="member.id">
+          <TableCell class="font-medium">
+            {{ member.id }}
+          </TableCell>
+          <TableCell>{{ member.name }}</TableCell>
+          <TableCell>{{ member.position }}</TableCell>
+          <TableCell>{{ member.departement }}</TableCell>
+          <TableCell class="text-right">
+            {{ member.departement }}
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  </main>
 </template>
