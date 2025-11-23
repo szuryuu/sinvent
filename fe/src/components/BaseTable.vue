d71a3c3ef4fc77f1ae3e4fb4429f84d2e775eb05
szuryuu <ilhamdzaky2007@gmail.com>
2025-11-23 20:57:41 +0700

    feat: badge

--- fe/src/components/BaseTable.vue
+++ fe/src/components/BaseTable.vue
@@ -3,6 +3,7 @@ import { Ellipsis } from 'lucide-vue-next'
 import BaseDelete from './BaseDelete.vue'
 import BaseEdit from './BaseEdit.vue'
 import { Button } from '@/components/ui/button'
+import { Badge } from '@/components/ui/badge'
 import {
   Table,
   TableBody,
@@ -37,11 +38,22 @@ defineProps({
   },
 })
 
+const statusColors = {
+  baik: 'bg-green-100 text-green-800',
+  dilelang: 'bg-yellow-100 text-yellow-800',
+  rusak: 'bg-red-100 text-red-800',
+  'tidak dipakai': 'bg-gray-100 text-gray-800',
+}
+
+const getStatusClass = (value) => {
+  return statusColors[value?.toLowerCase()] || 'bg-slate-200 text-slate-800'
+}
+
 function getCell(row, key) {
   if (!key.includes('.')) return row[key]
 
   return key.split('.').reduce((acc, part) => {
-    return acc?.[part] ?? null
+    return acc?.[part] ?? '-'
   }, row)
 }
 </script>
@@ -62,7 +74,23 @@ function getCell(row, key) {
         <TableCell class="font-medium">
           {{ index + 1 }}
         </TableCell>
-        <TableCell v-for="col in columns" :key="col.key"> {{ getCell(row, col.key) }}</TableCell>
+        <TableCell v-for="col in columns" :key="col.key" class="capitalize">
+          <template v-if="col.key === 'member.name'">
+            <Badge class="px-3 py-2 bg-cyan-100 text-cyan-800 rounded-xl">
+              {{ getCell(row, col.key) }}
+            </Badge>
+          </template>
+
+          <template v-else-if="col.key === 'status'">
+            <Badge class="px-3 py-2 rounded-xl" :class="getStatusClass(getCell(row, col.key))">
+              {{ getCell(row, col.key) }}
+            </Badge>
+          </template>
+
+          <template v-else>
+            {{ getCell(row, col.key) }}
+          </template>
+        </TableCell>
 
         <!-- Action -->
         <TableCell class="text-right">
